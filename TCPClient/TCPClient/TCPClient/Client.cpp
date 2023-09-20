cac83287b6b9a1217041734ade786be40792c8cd
yeobeomgu <beomgu1029@gmail.com>
2023-09-20 17:41:03 +0900

    TCP 이미지 파일 전송

--- TCPClient/TCPClient/TCPClient/Client.cpp
+++ TCPClient/TCPClient/TCPClient/Client.cpp
@@ -1,72 +1,52 @@
-﻿
-// 컴파일러에 경고 메시지를 표시하지 않도록 지시하는 프리프로세서 디렉티브
-#define _CRT_SECURE_NO_WARNINGS
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32")
 
 using namespace std;
 
 
-
-#include <WinSock2.h>
-
-#pragma comment(lib, "ws2_32")
-
 int main()
 {
-	//Windows 소켓 라이브러리를 초기화하는 부분.
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	//클라이언트 소켓을 생성
-	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, 0);
+	SOCKET ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	//서버 소켓의 주소 정보를 저장하는 구조체
-	SOCKADDR_IN ServerSock;
-	ZeroMemory(&ServerSock, 0);
+	SOCKADDR_IN ServerSockAddr;
+	memset(&ServerSockAddr, 0, sizeof(ServerSocket));
+	ServerSockAddr.sin_family = PF_INET;
+	ServerSockAddr.sin_port = htons(7777);
+	inet_pton(AF_INET, "127.0.0.1", &ServerSockAddr.sin_addr.s_addr);
 
-	ServerSock.sin_family = AF_INET;
-	ServerSock.sin_addr.s_addr = inet_addr("172.16.2.186");
-	ServerSock.sin_port = htons(7777);
+	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
 
-	// 서버에 연결을 시도, 연결 결과를 Reult에 저장
-	int Result = connect(ServerSocket, (SOCKADDR*)&ServerSock, sizeof(ServerSock));
-	if (Result == SOCKET_ERROR)
-	{
-		cout << "connect Error" << endl;
-		cout << "Socket Error Number : " << GetLastError() << endl;
-		exit(-1);
-	}
+	FILE* ReadFile = nullptr;
+	ReadFile = fopen("육전.jpg", "rb");
 
-	//서버로 부터 수신한ㄷ ㅔ이터를 저장하기 위한 Buffer
 	char Buffer[1024] = { 0, };
+	size_t ReadSize = 0;
 
-	//서버로부터 데이터를 수신
-	int RecvByte = recv(ServerSocket, Buffer, sizeof(Buffer), 0);
-	if (RecvByte <= 0)
+	do
 	{
-		cout << "recv Error" << endl;
-		cout << "Socket Error Number : " << GetLastError() << endl;
-		exit(-1);
-	}
+		ReadSize = fread(Buffer, sizeof(char), sizeof(Buffer), ReadFile);
+		size_t WriteSize = send(ServerSocket, Buffer, (int)ReadSize, 0);
+	} while (ReadSize > 0);
 
-	cout << "Recv : " << Buffer << endl;
+	//fseek(ReadFile, 0, SEEK_SET);
 
-	char Message[1024] = { 0, };
-	sprintf(Message, "%s Hellow", Buffer);
+	//do
+	//{
+	//	ReadSize = fread(Buffer, sizeof(char), sizeof(Buffer), ReadFile);
+	//	size_t WriteSize = send(ServerSocket, Buffer, (int)ReadSize, 0);
+	//} while (ReadSize > 0);
 
-	int SendByte = send(ServerSocket, Message, (int)strlen(Message), 0);
-	if (SendByte <= 0)
-	{
-		cout << "send Error" << endl;
-		cout << "Socket Error Number : " << GetLastError() << endl;
-		exit(-1);
-	}
+	fclose(ReadFile);
 
 	closesocket(ServerSocket);
 
 	WSACleanup();
-
 	return 0;
 }
